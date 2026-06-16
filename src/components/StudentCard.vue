3dbbd53cd2c8a82b75783dafba3db11e86cd58dc
RATHANA.SOKHA <RATHANA.SOKHA@PNC.LOCAL>
2026-06-16 13:10:15 +0700

    change some style

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -30,46 +30,20 @@ function viewDetail() {
 <style scoped>
 .student-card {
   display: flex;
-  /* justify-content: center; */
   margin: 20px;
 }
 
 .card {
   width: 280px;
-  /* background: white;
-  border-radius: 12px;
-  overflow: hidden;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.15);
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-  text-align: center;
-  padding-bottom: 15px; */
   border: 2px solid gray;
   text-align: center;
   padding-bottom: 15px;
 }
-
-/* .card:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 8px 18px rgba(0, 0, 0, 0.2);
-} */
-
 .card img {
   width: 100%;
   height: 220px;
-  object-fit: cover;
 }
 
-.card h3 {
-  margin: 15px 10px 8px;
-  font-size: 20px;
-  color: #333;
-}
-
-.card p {
-  margin: 0;
-  color: #666;
-  font-size: 16px;
-}
 button {
     margin-top: 10px;
     cursor: pointer;
